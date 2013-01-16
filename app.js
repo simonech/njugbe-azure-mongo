641a8e330482f29dff4fe1136d062f2d852ee139
Simone Chiaretta <simone@piyosailing.com>
2013-01-16 15:36:30 +0100

    tasklist

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express')
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path');
+var TaskList = require('./routes/tasklist');
+
+
+//var taskList = new TaskList(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
+var taskList = new TaskList("mongodb://njugbe:password@ds045077.mongolab.com:45077/njugbe");
+
+
+var app = express();
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.get('/', taskList.showTasks.bind(taskList));
+app.post('/addtask', taskList.addTask.bind(taskList));
+app.post('/completetask', taskList.completeTask.bind(taskList));
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
